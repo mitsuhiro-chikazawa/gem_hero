4cc95e0a5a419dd63162963f4cce787eb2319672
chika-tsubaru <k67e5b9fd7zh1@softbank.ne.jp>
2020-12-03 08:46:38 +0900

    create link top page

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,6 @@
 Rails.application.routes.draw do
   root 'news#index'
+  resources :news, only: [:index, :show]
   resources :character, only: [:index, :show]
   resources :creater, only: :index
 end
